08354a3c6cdcd55c5234ee55ea129d1305c10a45
GokulJaeger <gokuljaeger@gmail.com>
2021-02-18 17:17:34 +0530

    added car class assignment

--- src/main/java/com/jsfd/week1/day2/Automobile.java
+++ src/main/java/com/jsfd/week1/day2/Automobile.java
@@ -1,3 +1,32 @@
 package com.jsfd.week1.day2;
+// import java.util.Scanner;
 
+import java.util.Scanner;
 
+class Automobile{
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        // int n;
+        Boolean bool;
+
+        System.out.println("Welcome!");
+        System.out.println("Car Driving Simulation");
+        // System.out.println("Enter the number of vehicles");
+        // n = sc.nextInt();
+        
+        car obj= new car("Gokul", "Hyundai Verna", "OX0", 2020, 250, 17, 50);
+        // System.out.println(obj.getOwner()+" has "+obj.getName()+" "+obj.getModel()+" model which is made in "+obj.getYear()+", which has a top speed of "+obj.getSpeed()+"Kmph. It gives a milleage of "+obj.getMil()+"kmpl. The car holds  "+obj.getTank()+"l of tank capacity");
+        String str=obj.disp();
+        System.out.println(str);
+        System.out.println("Want to start the car?(True/False)");
+        bool=sc.nextBoolean();
+        obj.applyAcceleration(bool);
+
+        System.out.println("Want to stop the car?(True/False)");
+        bool=sc.nextBoolean();
+        obj.applyBrake(bool);
+        sc.close();  
+    }
+}
---
+++ src/main/java/com/jsfd/week1/day2/car.java
@@ -0,0 +1,129 @@
+package com.jsfd.week1.day2;
+import java.lang.ref.Cleaner;
+import java.math.*;
+
+public class car{
+    private String owner;
+    private String name;
+    private String model;
+    private int year;
+    private int speed;
+    private int mil;
+    private int tank;
+    private int ct=0;
+
+
+    public car(String owner, String name, String model, int year, int speed, int mil, int tank) {
+        this.owner = owner;
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.speed = speed;
+        this.mil = mil;
+        this.tank = tank;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getMil() {
+        return mil;
+    }
+
+    public void setMil(int mil) {
+        this.mil = mil;
+    }
+
+    public int getTank() {
+        return tank;
+    }
+
+    public void setTank(int tank) {
+        this.tank = tank;
+    }
+
+    public void applyAcceleration(Boolean isAcclApplied) {
+        int a=this.speed;
+        int b= (a*5)/18;
+        //System.out.println(this.speed);
+        System.out.println(b);
+		if (isAcclApplied) {
+            System.out.println("...The car is running...");
+            for(int i=0; i<=this.speed;i++){
+                if(i%b==0 || i%17==0){
+                    System.out.println("The car is moving at a speed of "+i+"mps");
+                    this.ct=i;
+                }
+            }
+            double x=(this.mil*this.tank)*0.25;
+			System.out.println("The car will cover the distance of "+x+"Kmph");
+		} 
+        else {
+            System.out.println("..The car is Idle..");
+        }
+			
+	}
+
+
+    public void applyBrake(Boolean isBrakeApplied) {
+        int a=this.speed;
+        int b=(a*5)/18;
+        int count=0;
+        System.out.println(this.ct);
+		if (isBrakeApplied) {
+            System.out.println("The car is slowing down!");
+            for(int i=this.ct;i>=0;i--){
+                if(i%b==0 || i%17==0){
+                System.out.println("The car is slowing down at a speed of "+i+"mps");
+                count+=1;
+                }
+            }
+			System.out.println("The car Stopped with in "+count+"seconds");
+		} 
+        else {
+			System.out.println("..The car is Still Running..");
+		}
+	} 
+
+    public String disp(){
+        return this.owner+" has "+this.name+" "+this.model+" model which is made in "+this.year+", which has a top speed of "+this.speed+"Kmph. It gives a milleage of "+this.mil+"kmpl. The car holds  "+this.tank+"l of tank capacity";
+    }
+
+}
