7dd54004ad2947d8672d2b339241cf64b0f4fcad
GokulJaeger <gokuljaeger@gmail.com>
2021-02-25 14:46:59 +0530

    CSV Sort updated

--- src/main/java/com/jsfd/week1/Exercises/Files.java
+++ src/main/java/com/jsfd/week1/Exercises/Files.java
@@ -1,5 +1,4 @@
-package com.jsfd.week1.day6_7;
-import java.io. * ;
+package com.jsfd.week1.Exercises;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,9 +8,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class CSVMultiThreads {
+public class Files {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader("D:/JFSD/week1/src/main/java/com/jsfd/week1/day6_7/List.csv"));
+        BufferedReader reader = new BufferedReader(new FileReader("D:/JFSD/week1/src/main/java/com/jsfd/week1/day6_7/Books.csv"));
         Map<String, List<String>> map = new TreeMap<String, List<String>>();
         String line = reader.readLine();//read header
         while ((line = reader.readLine()) != null) {
@@ -25,8 +24,8 @@ public class CSVMultiThreads {
 
         }
         reader.close();
-        FileWriter writer = new FileWriter("D:/JFSD/week1/src/main/java/com/jsfd/week1/day6_7/SortList.csv");
-        writer.write("Product, Customer, No.Unit, GPM,GPY,UnitPerDya,Vault,Product Type, Rate\n");
+        FileWriter writer = new FileWriter("D:/JFSD/week1/src/main/java/com/jsfd/week1/Exercises/SortList.csv");
+        writer.write("UserID, Module, Mark\n");
         for (List<String> list : map.values()) {
             for (String val : list) {
                 writer.write(val);
